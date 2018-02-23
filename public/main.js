c59172f3e406bede9605e12b297f25d690cc87af
Jakob Engberg-Pedersen <jakob@local-m.local>
2018-02-23 13:47:19 +0100

    first

---
+++ public/main.js
@@ -0,0 +1,26 @@
+(function() {
+	console.log("hello world")
+	const socket = io()
+
+
+	var audio=document.getElementById('audio');
+
+
+	const changeTime = (time) => {
+		audio.currentTime = time;
+		audio.play();
+	}
+
+
+	//$('audio').(function(){
+    //  socket.emit('chat message', $('#m').val());
+    //  $('#m').val('');
+    //  return false;
+    //});
+
+
+	socket.on("time", (msg) => {
+		console.log("got it " + msg)
+		changeTime(msg)
+	})
+})()
--- server.js
+++ server.js
@@ -2,8 +2,48 @@ const express = require("express")
 
 let app = express()
 
-app.get("/", (req,res) => {
-	res.send("hello")
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+let time = 0
+
+function repeat() {
+	time++
+	if(time>1341) {
+		time = 0
+	}
+	setTimeout(() => {
+		repeat()
+	}, 1000)
+}
+
+io.on('connection', function(socket){
+	console.log('a user connected')
+	socket.emit("time", time);
+	socket.on('disconnect', function(){
+	    console.log('user disconnected');
+  	})
+
 })
 
-app.listen(80)
+
+app.get("/", (req,res) => {
+	res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get("/main.js", (req,res) => {
+	res.sendFile(__dirname + "/public/main.js")
+})
+app.get("/socket.js", (req,res) => {
+	res.sendFile(__dirname + "/public/socket.js")
+})
+app.get("/jquery.js", (req,res) => {
+	res.sendFile(__dirname + "/public/jquery.js")
+})
+app.get("/dream", (req,res) => {
+	res.sendFile(__dirname + "/public/dream.mp3")
+})
+
+http.listen(80, () => {
+	repeat()
+})
